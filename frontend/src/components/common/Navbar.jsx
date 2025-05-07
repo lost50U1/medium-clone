da837a8404793bc171de737178f617303f65729b
Biruk Moges <birukmoges8@gmail.com>
2025-05-07 16:14:02 +0300

    Refactor the sign in and sign up modal to make it appear in all pages

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,41 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Logo } from "@/components/Logo";
 import { ModeToggle } from "./ModeToggle";
 import { useTheme } from "@/hooks/useTheme";
 import { useModals } from "@/hooks/useModals";
 import { Link } from "react-router";
+import { AuthModal } from "@/components/modals/AuthModal";
 
-// todo: the sign in and sign up only appears in the home page doesnt appear on other pages fix that
 export const Navbar = () => {
   const { theme } = useTheme();
-  const { openModal } = useModals();
+  const { state, closeModal, openModal } = useModals();
+
+  const [authView, setAuthView] = useState("signIn");
+
+  const handleOpen = state.signIn || state.signUp;
+  const handleClose = () => {
+    closeModal("signIn");
+    closeModal("signUp");
+  };
+
+  useEffect(() => {
+    if (state.signUp) setAuthView("signUp");
+    else if (state.signIn) setAuthView("signIn");
+  }, [state.signUp, state.signIn]);
+
   return (
-    <header className="max-w-7xl mx-auto w-full py-[18.5px] border-l border-r border-border px-4">
-      <div className="flex justify-between items-center">
-        <Link to="/" className="w-40 h-auto">
-          <Logo
-            color={
-              theme === "dark" || theme === "system" ? "#FFFFFF" : "#000000"
-            }
-          />
-        </Link>
-        <nav>
-          <div className="flex gap-4 items-center">
-            <Button
-              className="max-sm:hidden"
-              variant="secondary"
-              onClick={() => openModal("signIn")}
-            >
-              Sign In
-            </Button>
-            <Button onClick={() => openModal("signUp")}>Get Started</Button>
-            <div className="max-xs:hidden">
-              <ModeToggle />
+    <>
+      <header className="max-w-7xl mx-auto w-full py-[18.5px] border-l border-r border-border px-4">
+        <div className="flex justify-between items-center">
+          <Link to="/" className="w-40 h-auto">
+            <Logo
+              color={
+                theme === "dark" || theme === "system" ? "#FFFFFF" : "#000000"
+              }
+            />
+          </Link>
+          <nav>
+            <div className="flex gap-4 items-center">
+              <Button
+                className="max-sm:hidden"
+                variant="secondary"
+                onClick={() => openModal("signIn")}
+              >
+                Sign In
+              </Button>
+              <Button onClick={() => openModal("signUp")}>Get Started</Button>
+              <div className="max-xs:hidden">
+                <ModeToggle />
+              </div>
             </div>
-          </div>
-        </nav>
-      </div>
-    </header>
+          </nav>
+        </div>
+      </header>
+
+      <AuthModal
+        open={handleOpen}
+        onClose={handleClose}
+        view={authView}
+        onChangeView={setAuthView}
+      />
+    </>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,51 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
 import { Paths } from "@/router/Paths";
 import { useModals } from "@/hooks/useModals";
-import { AuthModal } from "@/components/modals/AuthModal";
 import { MainLayout } from "@/layouts/MainLayout";
 
 export const Home = () => {
-  const { state, openModal, closeModal } = useModals();
-
-  const [authView, setAuthView] = useState("signIn");
-
-  const handleOpen = state.signIn || state.signUp;
-  const handleClose = () => {
-    closeModal("signIn");
-    closeModal("signUp");
-  };
-
-  useEffect(() => {
-    if (state.signUp) setAuthView("signUp");
-    else if (state.signIn) setAuthView("signIn");
-  }, [state.signUp, state.signIn]);
+  const { openModal } = useModals();
 
   return (
-    <>
-      <MainLayout centered>
-        <h1 className="text-2xl md:text-6xl font-bold text-center mt-10 md:w-3/5">
-          Share your thoughts with the world
-        </h1>
-        <p className="mt-4 text-lg md:text-2xl text-center md:w-1/2 text-foreground/70">
-          LiteBlog is a minimalist blogging platform where ideas shine in the
-          dark.
-        </p>
-        <div className="flex gap-4 mt-8">
-          <Button onClick={() => openModal("signIn")}>Start Writing</Button>
-          <Button variant="secondary" asChild>
-            <Link to={Paths.blogs}>Read Blogs</Link>
-          </Button>
-        </div>
-      </MainLayout>
-
-      <AuthModal
-        open={handleOpen}
-        onClose={handleClose}
-        view={authView}
-        onChangeView={setAuthView}
-      />
-    </>
+    <MainLayout centered>
+      <h1 className="text-2xl md:text-6xl font-bold text-center mt-10 md:w-3/5">
+        Share your thoughts with the world
+      </h1>
+      <p className="mt-4 text-lg md:text-2xl text-center md:w-1/2 text-foreground/70">
+        LiteBlog is a minimalist blogging platform where ideas shine in the
+        dark.
+      </p>
+      <div className="flex gap-4 mt-8">
+        <Button onClick={() => openModal("signIn")}>Start Writing</Button>
+        <Button variant="secondary" asChild>
+          <Link to={Paths.blogs}>Read Blogs</Link>
+        </Button>
+      </div>
+    </MainLayout>
   );
 };
